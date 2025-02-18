bc8cc722785fcbf2aa93635a5e63ca23c468d3eb
Abdulghani Mikati <Abdulghani Mikati@WIN-KVQBAF92IK4>
2025-02-18 13:27:58 +0000

    Add project files.

---
+++ BlockchainAssignment/BlockchainApp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlockchainAssignment
+{
+    public partial class BlockchainApp : Form
+    {
+        public BlockchainApp()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        // Duplicate the value input into our new textbox (textBox1) in the large "console" (richTextBox1)
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = textBox1.Text;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
